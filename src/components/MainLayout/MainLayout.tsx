4e190a0166da5bcfa151948ede082950a44e97ea
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-12-08 01:59:37 +0700

    feat: add some components

---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,25 @@
+// MainLayout.tsx
+import React from 'react';
+import { Layout } from 'antd';
+import AppHeader from '../Header';
+import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
+
+const { Content } = Layout;
+
+export const MainLayout: React.FC = () => {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sidebar />
+      <Layout>
+        <AppHeader />
+        <Content style={{ margin: '16px' }}>
+          <div style={{ padding: 24, background: '#fff', minHeight: '100%' }}>
+            <Outlet />
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,63 @@
+// Sidebar.tsx
+import React from 'react';
+import { Layout, Menu, MenuProps } from 'antd';
+import {
+  UserOutlined,
+  HomeOutlined,
+  BookOutlined,
+} from '@ant-design/icons';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const { Sider } = Layout;
+
+
+const items = [
+  { 
+    key: '/', icon: <HomeOutlined />, label: 'Trang chủ' 
+  },
+  { 
+    key: '/users', icon: <UserOutlined />, label: 'Quản lí người dùng' 
+  },
+  {
+    key: 'books',
+    label: 'Quản lí sách',
+    icon: <BookOutlined />,
+    children: [
+      { key: '/books', label: 'Toàn bộ sách' },
+      { key: '/books/suppliers', label: 'Nhà cung cấp' },
+      { key: '/books/requests', label: 'Yêu cầu mượn sách' },
+      { key: '/books/addbook', label: 'Nhập sách mới' },
+    ],
+  },
+  {
+    key: '/change-password', label: 'Đổi mật khẩu', icon: <UserOutlined />
+  },
+  {
+    key: '/logout', label: 'Đăng xuất', icon: <UserOutlined />
+  }
+];
+export const Sidebar: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = (e: { key: string }) => {
+    navigate(e.key);
+  };
+
+  return (
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div className="logo" style={{ height: '32px', margin: '16px', color: 'white' }}>
+        My Logo
+      </div>
+      <Menu
+        onClick={handleClick}
+        selectedKeys={[location.pathname]}
+        defaultSelectedKeys={['/']}
+        mode="inline"
+        theme="dark"
+        items={items}
+      />
+    </Sider>
+  );
+};
+
