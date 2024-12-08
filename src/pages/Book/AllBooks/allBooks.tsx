f11caa3ee909a42166a614f747998391858db7ee
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-12-08 22:15:01 +0700

    feat: add allbook page

--- src/pages/Book/AllBooks/allBooks.tsx
+++ src/pages/Book/AllBooks/allBooks.tsx
@@ -1,7 +1,30 @@
 import { TableOutlined } from "@ant-design/icons";
-import { Button, Card, Divider, Table } from "antd";
+import { Button, Card, Divider, Input, Modal, Table } from "antd";
+import Search from "antd/es/input/Search";
+import { useState } from "react";
 
 export const AllBooks = () => {
+    const [isLoading, setLoading] = useState(false)
+
+    const modalText = {
+        'title': 'Thêm đầu sách mới',
+        'content': <div style={{  }}>
+            <Input placeholder="Tên đầu sách" style={{ width: 200 }} />
+            <Input placeholder="Tác giả" style={{ width: 200 }} />
+            <Input placeholder="Thể loại" style={{ width: 200 }} />
+            <Input placeholder="Số lượng" style={{ width: 200 }} />
+        </div>
+    }
+    const extra : JSX.Element = <>
+            <Search placeholder="tìm kiếm" style={{ width: 200 }} />
+            <Divider type="vertical" />
+            <Button 
+                onClick={() => Modal.info({title: modalText.title, content: modalText.content})}
+            >
+                Thêm đầu sách mới
+            </Button>
+        </>
+    
     const collumns = [
         {
             title: "ID",
@@ -22,7 +45,7 @@ export const AllBooks = () => {
             title: "Thể loại",
             dataIndex: "category",
             key: "category"
-        },        
+        },
         {
             title: "Số lượng",
             dataIndex: "quantity",
@@ -34,9 +57,9 @@ export const AllBooks = () => {
             key: "option",
             render: () => {
                 return <>
-                    <Button type="primary" icon={<TableOutlined />}>Xem</Button> 
-                    <Divider type="vertical" /> 
-                    <Button type="primary" icon={<TableOutlined />}>Sửa</Button>
+                    <Button type="primary" icon={<TableOutlined />} onClick={() => Modal.info({title: 'Thông tin đầu sách', content: 'thong tin'})}>Xem</Button>
+                    <Divider type="vertical" />
+                    <Button type="primary" icon={<TableOutlined />} onClick={() => Modal.info({title: modalText.title, content: modalText.content})}>Sửa</Button>
                 </>;
             }
         }
@@ -68,10 +91,9 @@ export const AllBooks = () => {
         }
     ]
     return <>
-        <Card title="Danh sách các đầu sách" bordered={false} style={{ width: '100%' }} extra={<Button>Thêm đầu sách mới</Button>}>
-            <Table columns={collumns} dataSource={dataSrc} />
+        <Card title="Danh sách các đầu sách" bordered={false} style={{ width: '100%' }} extra={extra}>
+            <Table columns={collumns} loading={isLoading} dataSource={dataSrc} />
         </Card>
     </>
 
 };
-  
