225d6db58d6d2c5bba39f315eaf1faeeb3953fb1
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-12-09 00:47:43 +0700

    feat: add borrow request page

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,6 +1,6 @@
 import AllBooks from "./AllBooks";
 import Suppliers from "./Suppliers";
-// import requests from "./requests";
-// import addbook from "./addbook";
+import Authors from "./Authors";
+import BorrowRequests from "./BorrowRequests";
 
-export { AllBooks, Suppliers };
+export { AllBooks, Suppliers, Authors, BorrowRequests };
