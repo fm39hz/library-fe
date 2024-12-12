332839c0521e9fb46b5dc8fef1c098445f06ec9d
FM39hz <hitpoint2k3@gmail.com>
2024-12-12 12:28:32 +0700

    chore: navigate to login after logout

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,62 +1,76 @@
 // Sidebar.tsx
-import React from 'react';
-import { Layout, Menu, MenuProps } from 'antd';
-import {
-  UserOutlined,
-  HomeOutlined,
-  BookOutlined,
-} from '@ant-design/icons';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { Layout, Menu, MenuProps } from "antd";
+import { UserOutlined, HomeOutlined, BookOutlined } from "@ant-design/icons";
+import { useNavigate, useLocation } from "react-router-dom";
+import authenticationApi from "../../services/api/authenticationApi";
 
 const { Sider } = Layout;
 
-
 const items = [
-  { 
-    key: '/', icon: <HomeOutlined />, label: 'Trang chủ' 
-  },
-  { 
-    key: '/users', icon: <UserOutlined />, label: 'Quản lí người dùng' 
+  {
+    key: "/",
+    icon: <HomeOutlined />,
+    label: "Trang chủ",
   },
   {
-    key: 'books',
-    label: 'Quản lí sách',
+    key: "/users",
+    icon: <UserOutlined />,
+    label: "Quản lí người dùng",
+  },
+  {
+    key: "books",
+    label: "Quản lí sách",
     icon: <BookOutlined />,
     children: [
-      { key: '/books', label: 'Toàn bộ sách' },
-      { key: '/books/suppliers', label: 'Nhà cung cấp' },
-      { key: '/books/authors', label: 'Tác giả' },
-      { key: '/books/requests', label: 'Yêu cầu mượn sách' },
+      { key: "/books", label: "Toàn bộ sách" },
+      { key: "/books/suppliers", label: "Nhà cung cấp" },
+      { key: "/books/authors", label: "Tác giả" },
+      { key: "/books/requests", label: "Yêu cầu mượn sách" },
       // { key: '/books/addbook', label: 'Nhập sách mới' },
     ],
   },
   {
-    key: '/subscription', label: 'Thành viên thân thiết', icon: <UserOutlined />
+    key: "/subscription",
+    label: "Thành viên thân thiết",
+    icon: <UserOutlined />,
   },
   {
-    key: '/change-password', label: 'Đổi mật khẩu', icon: <UserOutlined />
+    key: "/change-password",
+    label: "Đổi mật khẩu",
+    icon: <UserOutlined />,
   },
   {
-    key: '/logout', label: 'Đăng xuất', icon: <UserOutlined />
-  }
+    key: "/logout",
+    label: "Đăng xuất",
+    icon: <UserOutlined />,
+  },
 ];
 export const Sidebar: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const handleClick = (e: { key: string }) => {
+    if (e.key == "/logout") {
+      authenticationApi.logout();
+      navigate("/login");
+      return;
+    }
     navigate(e.key);
   };
 
   return (
     <Sider breakpoint="lg" collapsedWidth="0">
-      <div className="logo" style={{ height: '32px', margin: '16px', color: 'white' }}>
+      <div
+        className="logo"
+        style={{ height: "32px", margin: "16px", color: "white" }}
+      >
         My Logo
       </div>
       <Menu
         onClick={handleClick}
         selectedKeys={[location.pathname]}
-        defaultSelectedKeys={['/']}
+        defaultSelectedKeys={["/"]}
         mode="inline"
         theme="dark"
         items={items}
@@ -64,4 +78,3 @@ export const Sidebar: React.FC = () => {
     </Sider>
   );
 };
-
